8e5b81bd3526550505135fce6f760e30a1be068f
Zachary Streeter <Zachary.Streeter@amd.com>
2024-02-06 16:28:07 +0000

    added worflow yaml and refactored

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 A configuration-driven-AI-model project for testing.
 
 ## TODO
- - create testing
- - create docs
- - create more examples
- - handle hydra's output directory elegantly
+
+- create testing
+- create docs
+- create more examples
+- handle hydra's output directory elegantly
--- src/aiworkloads/scripts/main.py
+++ src/aiworkloads/scripts/main.py
@@ -18,13 +18,19 @@ from aiworkloads.scripts.utils import (
 @hydra.main(version_base=None, config_path="../configs", config_name="config")
 def main(cfg: DictConfig) -> None:
 
-    setup_paths(cfg)
-    generate_workload_script(cfg)
-    copy_workload_to_path(cfg)
-    generate_dockerfile(cfg)
-    build_save_image(cfg)
-    generate_job_schedular_script(cfg)
-    submit_job(cfg)
+    if cfg.workflow.setup_paths:
+        setup_paths(cfg)
+    if cfg.workflow.generate_workload_script:
+        generate_workload_script(cfg)
+    if cfg.workflow.copy_workload_to_path:
+        copy_workload_to_path(cfg)
+    if cfg.workflow.generate_image:
+        generate_dockerfile(cfg)
+        build_save_image(cfg)
+    if cfg.workflow.generate_job_schedular_script:
+        generate_job_schedular_script(cfg)
+    if cfg.workflow.submit_job:
+        submit_job(cfg)
 
 
 if __name__ == "__main__":
